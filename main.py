68551420f3d3103c18615b54e24facff6282d144
Edwin Granada <edwingran@utp.edu.co>
2025-10-07 18:39:21 -0500

    feat: agregar parámetro GET para elegir formato de hora (12h o 24h)

--- main.py
+++ main.py
@@ -17,11 +17,27 @@ country_timezones = {
 }
 
 @app.get("/time/{iso_code}")
-async def time(iso_code: str):
+async def time(iso_code: str, format: str = "24h"): # Parámetro tipo query
     iso = iso_code.upper()
     timezone_str = country_timezones.get(iso)
+    if not timezone_str:
+        return {"error": "Código de país no válido"}
+    
     tz = zoneinfo.ZoneInfo(timezone_str)
-    return{"time": datetime.now(tz)}
+    current_time = datetime.now(tz)
+    
+    # Definimos la lógica del formato
+    if format == "12h":
+        formatted_time = current_time.strftime("%I:%M:%S %p") #Ej: 07:30:15 PM
+    else:
+        formatted_time = current_time.strftime("%H:%M:%S") #Ej 19:30:15
+        
+    return {
+        "country": iso,
+        "time": formatted_time,
+        "format": format
+    }
+
 
 #Reto: crear un nuevo endpoint que reciba una variable en formato get 
 #Y que automaticamente se pueda hablitar el formato de hora. Por ejemplo devolver 
